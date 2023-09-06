e8717b716f772127e57e1b12071d04ec7bade3a9
Luciano Marani <lmarani@santafe.gov.ar>
2023-09-06 15:19:03 -0300

    Ex. 5.2

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,24 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
   event.preventDefault()
   try {
     const user = await loginService.login({
       username, password,
     })
-
+    window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      ) 
     blogService.setToken(user.token)
     setUser(user)
     setUsername('')
@@ -36,6 +47,12 @@ const App = () => {
     }, 5000)
   }
 }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('loggedBlogAppUser')
+    setUser(null)
+  }
 
   if (user === null) {
     return (
@@ -71,7 +88,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p> 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
